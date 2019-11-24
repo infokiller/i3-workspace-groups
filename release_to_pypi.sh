9f5903c0368a99ec0874519560e7782edb2d405b
Yoni Weill <joweill@icloud.com>
2019-11-24 18:08:34 +0200

    improve release_to_pypi

--- release_to_pypi.sh
+++ release_to_pypi.sh
@@ -3,5 +3,7 @@
 # See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -o errexit -o errtrace -o nounset -o pipefail
 
+cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+[[ -d dist ]] && rm -rf dist
 python setup.py sdist bdist_wheel
 twine upload --verbose dist/*

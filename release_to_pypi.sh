6b569e2667eb1b1b1a493ce36277b8ae46078f79
Yoni Weill <joweill@icloud.com>
2021-01-01 13:11:01 +0200

    updates

--- release_to_pypi.sh
+++ release_to_pypi.sh
@@ -3,7 +3,7 @@
 # See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -o errexit -o errtrace -o nounset -o pipefail
 
-cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+cd "$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 [[ -d dist ]] && rm -rf dist
 python setup.py sdist bdist_wheel
 twine upload --verbose dist/*

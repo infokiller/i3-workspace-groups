ca3b05c43a8b14bff0f8ee1b2d77a04609ead84a
Yoni Weill <joweill@icloud.com>
2024-01-13 13:13:34 +0200

    fix pypi release to use pyproject.toml

--- tools/release_to_pypi.sh
+++ tools/release_to_pypi.sh
@@ -3,7 +3,8 @@
 # See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -o errexit -o errtrace -o nounset -o pipefail
 
-cd "$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+cd "${DIR}/.."
 [[ -d dist ]] && rm -rf dist
-python setup.py sdist bdist_wheel
+python -m build
 twine upload --verbose dist/*
